a66a77961a067a58288713dc74fa401ae22e4557
nenastya <49988403+nenas1ya@users.noreply.github.com>
2022-01-27 09:26:23 +0500

    Update ryoko.py

--- ryoko.py
+++ ryoko.py
@@ -1,6 +1,6 @@
 import asyncio
 import requests
-from config import TGtok,YTtok, CHANEL_ID
+from config import TGtok,YTtok, CHANEL_ID, CHAT_ID
 from aiogram import Bot, executor, types, utils
 
 bot = Bot(TGtok)
@@ -14,19 +14,22 @@ print('uwu start')
 async def main():
     while True:
         try:
-            await asyncio.sleep(5)
+            await asyncio.sleep(360)
             api_url= 'https://www.googleapis.com/youtube/v3/search?'
-            search_url = f'''{api_url}key={YTtok}&channelId={CHANEL_ID}
-                            &part=snippet,id&order=date&maxResults=1'''
+            search_url = f'{api_url}key={YTtok}&channelId={CHANEL_ID}part=id&order=date&maxResults=1'
             r = requests.get(search_url)
             last_video = r.json()['items'][0]['id']['videoId']
             if last_video not in watched:
                 print(f'new vveedio ~ {last_video}')
                 link = f'https://youtu.be/{last_video}'
                 #decorated_link = utils.markdown.link(title='uwu',url=link) -> [title](url) ? кликабельный title
-                await send_message(-1001547382897, f"новое видево\n{link}")
+                await send_message(CHAT_ID, f"{link}")
                 watched.append(last_video)
-        except Exception as e: print(e)
+        except Exception as e:
+            print(e)
+            print(search_url)
+            print('-----')
+            pprint(r.json())
 
 
 loop = asyncio.get_event_loop()
